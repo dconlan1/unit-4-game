a4285dab75c87d4b60a77b079e9af180748f4292
Dave Conlan <daveconlan@me.com>
2019-10-20 19:32:19 -0700

    updating the html and js files. am having issues with .css

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+//set up a random number generator for the computer to pick a "random number". 
+//set up a calculation of how to match the sum of the crystal outputs to the "random number."
+//set up an assigned "number" for each crytal. 
+//
